607d6f363b5e9bff461f933a308c77046ad09577
lequocduy2k3 <21a100100063@students.hou.edu.vn>
2024-11-20 10:31:17 +0700

    thay đổi ngày 20/10

--- app/src/main/java/com/example/appmusic/activities/SplashActivity.java
+++ app/src/main/java/com/example/appmusic/activities/SplashActivity.java
@@ -7,18 +7,28 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.appmusic.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
     private Handler handler ;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         handler = new Handler();
+        mAuth = FirebaseAuth.getInstance();
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,RegisterActivity.class);
+                Intent intent = null;
+                if(mAuth.getCurrentUser() != null) {
+                    intent = new Intent(SplashActivity.this, RegisterActivity.class);
+                }else {
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                }
+                // Intent intent = new Intent(SplashActivity.this, RegisterActivity.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/example/appmusic/fragments/SignInFragment.java
+++ app/src/main/java/com/example/appmusic/fragments/SignInFragment.java
@@ -1,5 +1,6 @@
 package com.example.appmusic.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,18 +8,32 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.appmusic.R;
+import com.example.appmusic.activities.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SignInFragment extends Fragment {
     private TextView resetPassword , donHaveAnAccount;
     private FrameLayout frameLayout ;
+    private EditText email , password;
+    private Button signInButton;
+    private FirebaseAuth mAuth;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,6 +44,14 @@ public class SignInFragment extends Fragment {
         donHaveAnAccount = view.findViewById(R.id.dont_have_an_account);
         resetPassword = view.findViewById(R.id.reset_password);
         frameLayout = getActivity().findViewById(R.id.register_frame_layout);
+
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+        signInButton = view.findViewById(R.id.signInButton);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         return view;
     }
 
@@ -49,9 +72,92 @@ public class SignInFragment extends Fragment {
                 setFragment(new ForgotPasswordFragment());
             }
         });
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInButton.setEnabled(false);
+                signInButton.setTextColor(getResources().getColor(R.color.color_text_artist));
+                signInWithFireBase();
+
+            }
+        });
+    }
+
+    private void signInWithFireBase() {
+        if(email.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+\\.[a-z]+")){
+            mAuth.signInWithEmailAndPassword(email.getText().toString() , password.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(getActivity(), MainActivity.class);
+                                getActivity().startActivity(intent);
+                                getActivity().finish();
+                            }else {
+                                Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                signInButton.setEnabled(true);
+                                signInButton.setTextColor(getResources().getColor(R.color.white));
+                            }
+                        }
+                    });
+        }else {
+            email.setError("Email không hợp lệ");
+            signInButton.setEnabled(true);
+            signInButton.setTextColor(getResources().getColor(R.color.white));
         }
+    }
+
+    private void checkInput() {
+        if(!email.getText().toString().isEmpty()){
+            if(!password.getText().toString().isEmpty()){
+                signInButton.setEnabled(true);
+                signInButton.setTextColor(getResources().getColor(R.color.white));
+            }else {
+                signInButton.setEnabled(false);
+                signInButton.setTextColor(getResources().getColor(R.color.color_text_artist));
+            }
+        }else {
+            signInButton.setEnabled(false);
+            signInButton.setTextColor(getResources().getColor(R.color.color_text_artist));
+        }
+    }
+
     private void setFragment(Fragment fragment){
         FragmentTransaction fragmentTransaction =getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.form_right , R.anim.out_from_left);
         fragmentTransaction.replace(frameLayout.getId(),fragment);
         fragmentTransaction.commit();
     }
